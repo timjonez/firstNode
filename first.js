7855a553e04869934deb7e75869295ccef55b697
timjonez <timjonez@protonmail.com>
2020-10-30 07:52:23 -0500

    [first.js] add basic server

---
+++ first.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello world')
+});
+
+server.listen(port, hostname, () => {
+	console.log(`server running at http://${hostname}:${port}/`);
+});
